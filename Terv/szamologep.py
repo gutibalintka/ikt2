24887645dc89e6b121d528e090b75a34a34224c9
gutibalintka <gutibalint@turr.hu>
2023-05-31 14:14:04 +0200

    Kód folytatás

--- Terv/szamologep.py
+++ Terv/szamologep.py
@@ -1,1 +1,9 @@
 from tkinter import*
+
+abl1=Tk()
+
+abl1=Text("Üdvözlünk az EKG(B) számológépben! \n Mielőtt elkezdenéd használni a programunkat, szeretnénk, ha válaszolnál egy kérdésre!")
+abl2=Text("")
+abl3=Text("")
+
+abl1.mainloop()
